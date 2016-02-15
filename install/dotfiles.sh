5f1b5cbf01c2fb7ebe0a5df0473768a783d43358
Neil Grogan <neil@ngrogan.com>
2016-02-15 18:57:59 +0000

    Updated to try fix find for osx 4

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -21,7 +21,7 @@ function get_os() {
 
 function link_file_list(){
 	if is_osx; then
-		find "$link_dir" -print0 | xargs -0 stat -f '%i '
+		find "$link_dir" -mindepth 1 -maxdepth 1 -print0
 	else
 		find "$link_dir" -mindepth 1 -maxdepth 1 -printf "%P\n"
 	fi	
