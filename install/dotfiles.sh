27f7bcf9ed23141edd77a603ec4cb03742749a32
Neil Grogan <neil@grogan.ie>
2016-02-15 20:35:21 +0000

    trying to fix linking command

--- install/dotfiles.sh
+++ install/dotfiles.sh
@@ -38,5 +38,5 @@ function unlink_files(){
 	file_list "$1" | while read -r file; do echo "Will unlink $file to home"; "$2" "$HOME/$file"; done
 }
 
-link_files "link" "ln -sfn"
+link_files "link" 'ln -sfn'
 #unlink_files "link" "unlink"
