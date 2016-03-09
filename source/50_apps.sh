8f03f62a850fd8e8c0a5cee26341c329e9dca1c9
Neil Grogan <neil@ngrogan.com>
2016-03-09 14:51:11 +0000

    Moved Docker aliases to more general apps file

--- source/50_apps.sh
+++ source/50_apps.sh
@@ -104,3 +104,23 @@ for al in `__git_aliases`; do
     function_exists $complete_fnc && __git_complete g$al $complete_func
 done
 # }}}
+
+# Docker commands {{{
+alias d="docker"
+alias di='docker images'
+alias dps='docker ps'
+alias dim='docker images'
+alias dka='docker kill $(docker ps -q --no-trunc)'
+alias dsta='docker stop $(docker ps -q --no-trunc)'
+alias drmae='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
+alias drmiad='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
+alias dr='docker run'
+alias dritrm='docker run -it --rm'
+#alias dr='docker run -it --rm'
+alias dpsa='docker ps -a'
+alias din='docker inspect'
+
+function d(){
+  docker "$@"
+}
+# }}}
