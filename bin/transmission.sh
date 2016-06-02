a0733009e114a1cec953a7d1dabf353a10476d8c
Neil Grogan <neil@grogan.ie>
2016-06-02 19:48:35 +0100

    updated

--- bin/transmission.sh
+++ bin/transmission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MOVE_DIR="$HOME/Documents/Downloads/ForiTunes"
+MOVE_DIR="$HOME/Documents/Downloads/Complete"
 
 transmission-remote -t $TORRENTID --move $MOVE_DIR
 transmission-remote -t $TORRENTID --remove
