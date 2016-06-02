c7d7f7052c3fac5bf2abeb884080de4b553384a7
Neil Grogan <neil@grogan.ie>
2016-06-02 23:24:01 +0100

    added quotes

--- bin/transmission.sh
+++ bin/transmission.sh
@@ -2,5 +2,5 @@
 
 MOVE_DIR="$HOME/Documents/Downloads/Complete"
 
-transmission-remote -t $TR_TORRENT_ID --move $MOVE_DIR
-transmission-remote -t $TR_TORRENT_ID --remove
+transmission-remote -t "$TR_TORRENT_ID" --move "$MOVE_DIR"
+transmission-remote -t "$TR_TORRENT_ID" --remove
