19d1db3e6ba3ee78500efeab98773b3c8dc5af15
Neil Grogan <neil@grogan.ie>
2016-11-06 23:05:03 +0000

    Only pull repo if chnages exist - nfsn

--- bin/nfsn.hugo.sh
+++ bin/nfsn.hugo.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 # A Scrit to do a hugo build if any future posts exist
 set -x
+
 cd $HOME/repos/ngrogan.com
-git pull
+
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse origin)
+
+if [ $LOCAL = $REMOTE ]; then
+    ;
+else
+	git pull
+fi
+
 ~/bin/hugo -d $DOCUMENT_ROOT
