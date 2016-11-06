71f564ed5882c04bfd65b2ef52a048d7d110f88f
Neil Grogan <neil@grogan.ie>
2016-11-06 23:47:35 +0000

    Updating hugo script to build if any dates in the future

--- bin/nfsn.hugo.sh
+++ bin/nfsn.hugo.sh
@@ -12,5 +12,13 @@ if [ $LOCAL = $REMOTE ]; then
 else
 	git pull
 fi
+grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" -R content | grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" | while read line; do
+  todate=$(date -d \"$line\" +"%Y%m%d")  # = 20130718
+  cond=$(date +"%Y%m%d")    # = 20130715
 
-~/bin/hugo -d $DOCUMENT_ROOT
+  if [ $todate -ge $cond ]; #put the loop where you need it
+  then
+     ~/bin/hugo -d $DOCUMENT_ROOT
+     exit 0
+  fi
+done
