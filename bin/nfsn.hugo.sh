bec9d1d7e9eff4b3469dfc51c598d245d27933c9
Neil Grogan <neil@grogan.ie>
2016-11-06 23:57:04 +0000

    Updated NFSN hugo script to only generate if needed

--- bin/nfsn.hugo.sh
+++ bin/nfsn.hugo.sh
@@ -12,12 +12,16 @@ if [ $LOCAL = $REMOTE ]; then
 else
 	git pull
 fi
+NEEDS_UPDATE="false"
 grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" -R content | grep -o "[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}" | sed -e 's/\-//g' | while read line; do
   cond=$(date +"%Y%m%d")    # = 20130715
 
   if [ $line -ge $cond ]; #put the loop where you need it
   then
-     ~/bin/hugo -d $DOCUMENT_ROOT
-     exit 0
+    NEEDS_UPDATE="true"
   fi
 done
+
+if [[ "$NEEDS_UPDATE" = true ]]; then
+  ~/bin/hugo -d $DOCUMENT_ROOT
+fi
