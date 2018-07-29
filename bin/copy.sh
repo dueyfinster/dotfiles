ee5413e8e5be897c9d594f2caa6eee0933c50a58
Neil Grogan <neil@grogan.ie>
2018-07-29 15:44:53 +0100

    Removed unused scripts

--- bin/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [[ ! "$(type -P rsync)" ]]; then
-  echo "RSync must be installed first."
-  exit 1
-fi
-
-rsync -avz "$HOME"/Dropbox/Apps/dotfiles/ "$HOME"/.dotfiles/
--- bin/transmission.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TRANS_BIN="/usr/local/bin/transmission-remote"
-
-MOVE_DIR="$HOME/Documents/Downloads/Complete"
-
-"$TRANS_BIN" -t "$TR_TORRENT_ID" --move "$MOVE_DIR"
-sleep 2
-"$TRANS_BIN" -t "$TR_TORRENT_ID" --remove
