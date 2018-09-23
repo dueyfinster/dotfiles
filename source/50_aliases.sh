5375117d3f38d254bb0863e8947819e4a351ed67
Neil Grogan <neil@grogan.ie>
2018-09-23 16:54:27 +0100

    Cleaned up Aliases

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -27,12 +27,14 @@ function_exists() {
     return $?
 }
 
-for al in `__git_aliases`; do
-    alias g$al="git $al"
+if [ -n "$(type -t __git_aliases)" ]; then
+  for al in `__git_aliases`; do
+      alias g$al="git $al"
 
-    complete_func=_git_$(__git_aliased_command $al)
-    function_exists $complete_fnc && __git_complete g$al $complete_func
-done
+      complete_func=_git_$(__git_aliased_command $al)
+      function_exists $complete_fnc && __git_complete g$al $complete_func
+  done
+fi
 # }}}
 
 # Shell & System {{{
