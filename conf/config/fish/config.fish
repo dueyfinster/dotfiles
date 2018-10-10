56038b119fe623ea6a6f5be9933ec60672fd45fa
Neil Grogan <neil@grogan.ie>
2018-10-10 20:35:55 +0100

    Updated fish config to add git abbrs

--- conf/config/fish/config.fish
+++ conf/config/fish/config.fish
@@ -1,3 +1,17 @@
+# Turn off fish greeting
+set -e  fish_greeting
+
 # load solarized colors
 set -g theme_color_scheme solarized-dark
 set -g __fish_git_prompt_show_informative_status 1
+
+# Abbreviations
+abbr -a e 'exit'
+abbr -a g 'git'
+abbr -a ga 'git add'
+abbr -a gc 'git commit -m'
+abbr -a gd 'git diff'
+abbr -a gl 'git log'
+abbr -a gs 'git status'
+abbr -a gp 'git push'
+abbr -a gpl 'git pull'
