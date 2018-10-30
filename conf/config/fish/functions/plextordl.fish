0ce5899e8a44a820e301f908c3bbccb046c473a7
Neil Grogan <neil@grogan.ie>
2018-10-30 11:42:40 +0000

    Update plextordl.fish

--- conf/config/fish/functions/plextordl.fish
+++ conf/config/fish/functions/plextordl.fish
@@ -1,4 +1,4 @@
 function plextordl --description 'Download a video from youtube to plex'
-  token=(cat ~/.webhook)
+  set token (cat ~/.webhook)
   curl -X POST --data "token=$token" --data "type=$argv[1]" --data "uri=$argv[2]" https://ballyda.com:8080/hooks/add-torrent
 end
