7682504967b165e57cb64825937cd8e50154bc3e
Neil Grogan <neil@grogan.ie>
2018-10-30 11:39:18 +0000

    Update plexytdl.fish

--- conf/config/fish/functions/plexytdl.fish
+++ conf/config/fish/functions/plexytdl.fish
@@ -1,4 +1,4 @@
 function plexytdl --description 'Download a video from youtube to plex'
   token=(cat ~/.webhook)
-  curl -X POST --data "token=$token" --data "type=$argv[1]" --data "uri=$argv[2]" https://ballyda.com/hooks/yt-dl
+  curl -X POST --data "token=$token" --data "type=$argv[1]" --data "uri=$argv[2]" https://ballyda.com:8080/hooks/yt-dl
 end
