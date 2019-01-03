e72d972dd4290bf4ca2b442a9b3bf2a9d88d9bc4
Neil Grogan <neil@grogan.org>
2019-01-03 14:09:25 +0000

    Update dyncf.sh

--- bin/dyncf.sh
+++ bin/dyncf.sh
@@ -3,7 +3,6 @@
 
 [[ $# -lt 3 ]] && echo "Usage: $0 USERNAME PASSWORD ZONE DNSRECORD" && exit 1
 
-COOKIEJAR=/tmp/cf_cookie.txt
 USERNAME=${1}
 PASSWORD=${2}
 zone=${3}
@@ -32,9 +31,18 @@ dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid
 
 echo "DNSrecordid for $dnsrecord is $dnsrecordid"
 
+current_ip4=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
+  -H "X-Auth-Email: $USERNAME" \
+  -H "X-Auth-Key: $PASSWORD" \
+  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .content')
+  
+echo "Current ip4 on cloudflare is: $current_ip4"
+
 # update the record
+if [ "$ip" != "$current_ip4" ]; then
 curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
   -H "X-Auth-Email: $USERNAME" \
   -H "X-Auth-Key: $PASSWORD" \
   -H "Content-Type: application/json" \
   --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
+fi
