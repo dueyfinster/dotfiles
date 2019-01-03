7def7de38263b74b279ae01756dbc6bf24044511
Neil Grogan <neil@grogan.org>
2019-01-03 14:32:01 +0000

    Update dyncf.sh

--- bin/dyncf.sh
+++ bin/dyncf.sh
@@ -8,43 +8,70 @@ PASSWORD=${2}
 zone=${3}
 dnsrecord=${4}
 
+function get_ipv4(){
+  curl -s -X GET https://checkip.amazonaws.com | echo
+}
 
-# Get the current external IP address
-ip=$(curl -s -X GET https://checkip.amazonaws.com)
+function get_ipv6(){
+  # TODO
+  curl -s -X GET https://checkip.amazonaws.com | echo
+}
 
-echo "Current IP is $ip"
-# if here, the dns record needs updating
+function get_zone_id(){
+  curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$1&status=active" \
+    -H "X-Auth-Email: $2" \
+    -H "X-Auth-Key: $3" \
+    -H "Content-Type: application/json" \
+    | jq -r '{"result"}[] | .[0] | .id' \
+    | echo
+}
 
-# get the zone id for the requested zone
-zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
-  -H "X-Auth-Email: $USERNAME" \
-  -H "X-Auth-Key: $PASSWORD" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
+function get_record_id(){
+  curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$1/dns_records?type=$2&name=$dnsrecord" \
+    -H "X-Auth-Email: $4" \
+    -H "X-Auth-Key: $5" \
+    -H "Content-Type: application/json" \
+    | jq -r '{"result"}[] | .[0] | .id' \
+    | echo
+}
 
-echo "Zoneid for $zone is $zoneid"
-
-# get the dns record id
-dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
-  -H "X-Auth-Email: $USERNAME" \
-  -H "X-Auth-Key: $PASSWORD" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
-
-echo "DNSrecordid for $dnsrecord is $dnsrecordid"
-
-current_ip4=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
-  -H "X-Auth-Email: $USERNAME" \
-  -H "X-Auth-Key: $PASSWORD" \
-  -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .content')
-  
-echo "Current ip4 on cloudflare is: $current_ip4"
-
-# update the record
-if [ "$ip" != "$current_ip4" ]; then
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
-  -H "X-Auth-Email: $USERNAME" \
-  -H "X-Auth-Key: $PASSWORD" \
+function get_record_value(){
+  curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$1/dns_records?type=$2&name=$3" \
+  -H "X-Auth-Email: $4" \
+  -H "X-Auth-Key: $5" \
   -H "Content-Type: application/json" \
-  --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
-else
-  echo "Two IP Address are the same, no changes needed."
-fi
+  | jq -r '{"result"}[] | .[0] | .content' \
+  | echo
+}
+
+function update_record_value(){
+  curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$1/dns_records/$2" \
+  -H "X-Auth-Email: $3" \
+  -H "X-Auth-Key: $4" \
+  -H "Content-Type: application/json" \
+  --data "{\"type\":\"$5\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
+}
+
+function main(){
+  # Get the current external IP address
+  ip=$()
+  echo "Current IP is $ip"
+  
+  zoneid=$(get_zone_id $zone $USERNAME $PASSWORD)
+  echo "Zoneid for $zone is $zoneid"
+  
+  dnsrecordid=$(get_record_id $zoneid A $dnsrecord $USERNAME $PASSWORD)
+  echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+
+  current_ip4=$(get_record_value $zoneid A $dnsrecord $USERNAME $PASSWORD)
+  echo "Current ip4 on cloudflare is: $current_ip4"
+  
+  # update the record
+  if [ "$ip" != "$current_ip4" ]; then
+    update_record_value $zoneid $dnsrecordid $USERNAME $PASSWORD A $ip
+  else
+    echo "The Two IP Address are the same, no changes needed."
+  fi
+}
+
+main 
