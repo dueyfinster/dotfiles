c0a5f4d99616872dcc431d0ea65ac7c9eeb5dc63
Neil Grogan <neil@grogan.ie>
2019-01-03 15:39:55 +0000

    Fixed cloudflare dyndns script to do ipv6 also

--- bin/dyncf.sh
+++ bin/dyncf.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # A script to dynamically update DNS on Cloudflare
-
 [[ $# -lt 3 ]] && echo "Usage: $0 USERNAME PASSWORD ZONE DNSRECORD" && exit 1
 
 USERNAME=${1}
@@ -8,14 +7,12 @@ PASSWORD=${2}
 zone=${3}
 dnsrecord=${4}
 
-
-function get_ipv4(){
-  curl -s -X GET https://checkip.amazonaws.com
-}
-
-function get_ipv6(){
-  # TODO
-  curl -s -X GET https://checkip.amazonaws.com
+function get_ip(){
+  if [ "$1" == "A" ]; then
+    curl -4 -s -X GET https://checkip.amazonaws.com
+  else
+    curl -6 -s -X GET https://ifconfig.co
+  fi
 }
 
 function request(){
@@ -35,27 +32,28 @@ function update_record_value(){
 }
 
 function main(){
-  # Get the current external IP address
-  ip=$(get_ipv4)
-  echo "Current IP is $ip"
+  for iptype in AAAA A; do
+    # Get the current external IP address
+    ip=$(get_ip $iptype)
+    echo "Current IP is $ip"
 
-  zoneid=$(request "?name=$zone&status=active" ".id")
-  echo "Zoneid for $zone is $zoneid"
+    zoneid=$(request "?name=$zone&status=active" ".id")
+    echo "Zoneid for $zone is $zoneid"
 
 
-  dnsrecordid=$(request "$zoneid/dns_records?type=A&name=$dnsrecord" ".id")
-  echo "DNSrecordid for $dnsrecord is $dnsrecordid"
+    dnsrecordid=$(request "$zoneid/dns_records?type=$iptype&name=$dnsrecord" ".id")
+    echo "DNSrecordid for $dnsrecord is $dnsrecordid"
 
-  current_ip4=$(request "$zoneid/dns_records?type=A&name=$dnsrecord" ".content")
-  echo "Current ip4 on cloudflare is: $current_ip4"
+    current_ip=$(request "$zoneid/dns_records?type=$iptype&name=$dnsrecord" ".content")
+    echo "Current $iptype on cloudflare is: $current_ip"
 
-  # update the record
-  if [ "$ip" != "$current_ip4" ]; then
-    update_record_value $zoneid $dnsrecordid $USERNAME $PASSWORD A $ip
-  else
-    echo "The Two IP Address are the same, no changes needed."
-  fi
-  exit 0
+    # update the record
+    if [ "$ip" != "$current_ip" ]; then
+      update_record_value $zoneid $dnsrecordid $USERNAME $PASSWORD $iptype $ip
+    else
+      echo "The Two IP Address are the same, no changes needed."
+    fi
+  done
 }
 
 main
