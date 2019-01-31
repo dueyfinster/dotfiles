1924ee6d5b3d081309e9001a61c5846e29d7a0f1
Neil Grogan <neil.grogan@ericsson.com>
2019-01-31 14:01:17 +0000

    Fixed ripgrep version in script

--- init/50_ubuntu_install_ripgrep.sh
+++ init/50_ubuntu_install_ripgrep.sh
@@ -1,7 +1,7 @@
 # Ubuntu-only stuff. Abort if not Ubuntu.
 is_ubuntu || return 1
 
-RG_VERSION="${1:-0.10.0}"
+RG_VERSION="0.10.0"
 
 ignore_output() {
   "$@" > /dev/null 2>&1
@@ -9,7 +9,7 @@ ignore_output() {
 
 binary_name() {
   local version="$1"
-  echo "ripgrep-${version}_amd64.deb"
+  echo
 }
 
 download_binary() {
@@ -31,13 +31,14 @@ downloaded_binary() {
 
 install_linux() {
   local version="$1"
+  local bin_name="ripgrep-${version}_amd64.deb"
 
   if ! downloaded_binary $version; then
     echo "Installing rg v$version"
 
     tmp_dir=$(mktemp --directory)
 
-    download_binary "$version" "$tmp_dir"
+    download_binary "$version" "$bin_name" "$tmp_dir"
 
     sudo dpkg -i "$tmp_dir"/*.deb
 
@@ -45,14 +46,35 @@ install_linux() {
   fi
 }
 
+install_windows() {
+  local version="$1"
+  local bin_name="ripgrep-${version}-x86_64-pc-windows-msvc.zip"
+
+  if ! downloaded_binary $version; then
+    echo "Installing rg v$version"
+
+    tmp_dir=$(mktemp --directory)
+
+    download_binary "$version" "$bin_name" "$tmp_dir"
+
+    unzip "$tmp_dir"/*.zip
+
+    #TODO move binary to correct location
+
+    rm -rf $tmp_dir
+  fi
+}
+
 main() {
-  local version="${1:-$RG_VERSION}"
+  local version="$RG_VERSION"
   local dist="$(uname -s)"
 
   case "$dist" in
     Linux)
       install_linux "$version"
       ;;
+    MINGW64_NT-10.0)
+      install_windows "$version"
     *)
       echo "Don't know how to install rg on $dist"
       exit 1
