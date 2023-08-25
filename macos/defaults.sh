8a53b82b3c9f7f6d0171e36ef75370c14197c3e7
Neil Grogan <neil@grogan.org>
2023-08-25 16:13:12 +0100

    Added mac defaults and stow ignore file

---
+++ macos/defaults.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# Terminal                                                                    #
+###############################################################################
+
+# Only use UTF-8 in Terminal.app
+defaults write com.apple.terminal StringEncodings -array 4
+
+# Appearance
+defaults write com.apple.terminal "Default Window Settings" -string "Pro"
+defaults write com.apple.terminal "Startup Window Settings" -string "Pro"
+defaults write com.apple.Terminal ShowLineMarks -int 0
+
+###############################################################################
+# Software Updates                                                            #
+###############################################################################
+
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates weekly (`dot update` includes software updates)
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -string 7
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -bool true
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -bool true
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
+# Allow the App Store to reboot machine on macOS updates
+defaults write com.apple.commerce AutoUpdateRestartRequired -bool true
+
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+#for app in "Address Book" "Calendar" "Contacts" "Dock" "Finder" "Mail" "Safari" "SystemUIServer" "iCal"; do
+  #killall "${app}" &> /dev/null
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
