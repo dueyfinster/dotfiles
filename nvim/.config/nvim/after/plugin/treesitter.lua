98d08220127939a160755de332d56e6e3f28c538
Neil Grogan <neil@grogan.org>
2024-02-05 19:09:39 +0000

    added heex and eex grammer to nvim treesitter

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "elixir", "javascript", "vim", "vimdoc", "query" },
+  ensure_installed = { "c", "lua", "elixir", "eex", "heex",  "javascript", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
