0fd6c132e0243cd1ea7acb8fe113622c29fc5c03
Neil Grogan <neil@grogan.org>
2024-04-17 17:24:27 +0100

    Neovim: fix format of lazy init

--- nvim/.config/nvim/lua/df/lazy_init.lua
+++ nvim/.config/nvim/lua/df/lazy_init.lua
@@ -12,6 +12,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "df.lazy",
-    change_detection = { notify = false }
+  spec = "df.lazy",
+  change_detection = { notify = false }
 })
