6abfc44996af69c296005b6b99ff0782b8d91d48
Neil Grogan <neil@grogan.org>
2024-04-17 11:12:55 +0100

    Neovim: Packer to Lazy package manager

--- nvim/.config/nvim/lua/df/remap.lua
+++ nvim/.config/nvim/lua/df/remap.lua
@@ -45,7 +45,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 -- make executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/df/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/df/lazy.lua<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
   vim.cmd("so")
