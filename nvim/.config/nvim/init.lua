cf80acc5e586fd8e005f3bd4b9feb0716dded415
Neil Grogan <neil@grogan.org>
2025-08-17 15:43:36 +0100

    switch to lazyvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,2 @@
-require("df")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- nvim/.config/nvim/lua/df/lazy/telescope.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "nvim-telescope/telescope.nvim",
-
-  tag = "0.1.5",
-
-  dependencies = {
-    "nvim-lua/plenary.nvim"
-  }
-}
