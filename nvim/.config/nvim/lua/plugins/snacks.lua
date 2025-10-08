70f0357d811567185603014ddd9076bd7f953c58
Neil Grogan <neil@grogan.org>
2025-10-08 16:57:49 +0100

    Lazyvim: Change hidden files in snacks.nvim

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- for hidden files
+        ignored = true, -- for .gitignore files
+        sources = {
+          files = { hidden = true },
+        },
+      },
+    },
+  },
+}
